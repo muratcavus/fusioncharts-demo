3a1d95ac78cb46d925daebf51f29acf0492430a6
Murat Cavus <muratcavus@outlook.com>
2017-12-17 13:00:01 +0300

    pretified js methods

--- src/fusioncharts_javafx/Fusioncharts_javafx.java
+++ src/fusioncharts_javafx/Fusioncharts_javafx.java
@@ -71,47 +71,78 @@ public class Fusioncharts_javafx extends Application {
 				try {
 					urlFusionchartsjs = fileFusionchartsjs.toURI().toURL();
 					urlFusionchartsthemejs = fileFusionchartsthemejs.toURI().toURL();
-					engine.loadContent("<html>\n" + "<head>\n"
+					engine.loadContent("<html>\n" 
+							+ "<head>\n"
 							+ "<title>My first chart using FusionCharts Suite XT</title>\n"
-							+ "<script type=\"text/javascript\" src=\"" + urlFusionchartsjs.toString()
-							+ "\"></script>\n" + "<script type=\"text/javascript\" src=\""
-							+ urlFusionchartsthemejs.toString() + "\"></script>\n"
-							+ "<script type=\"text/javascript\">\n" + "FusionCharts.ready(function () {\n"
-							+ "    var revenueChart = new FusionCharts({\n" + "        type: 'doughnut2d',\n"
-							+ "        renderAt: 'chart-container',\n" + "        width: '450',\n"
-							+ "        height: '450',\n" + "        dataFormat: 'json',\n" + "        dataSource: {\n"
-							+ "            \"chart\": {\n"
-							+ "                \"caption\": \"Split of Revenue by Product Categories\",\n"
-							+ "                \"subCaption\": \"Last year\",\n"
-							+ "                \"numberPrefix\": \"$\",\n"
-							+ "                \"paletteColors\": \"#0075c2,#1aaf5d,#f2c500,#f45b00,#8e0000\",\n"
-							+ "                \"bgColor\": \"#ffffff\",\n" + "                \"showBorder\": \"0\",\n"
-							+ "                \"use3DLighting\": \"0\",\n" + "                \"showShadow\": \"0\",\n"
-							+ "                \"enableSmartLabels\": \"0\",\n"
-							+ "                \"startingAngle\": \"310\",\n"
-							+ "                \"showLabels\": \"0\",\n"
-							+ "                \"showPercentValues\": \"1\",\n"
-							+ "                \"showLegend\": \"1\",\n" + "                \"legendShadow\": \"0\",\n"
-							+ "                \"legendBorderAlpha\": \"0\",\n"
-							+ "                \"defaultCenterLabel\": \"Total revenue: $64.08K\",\n"
-							+ "                \"centerLabel\": \"Revenue from $label: $value\",\n"
-							+ "                \"centerLabelBold\": \"1\",\n"
-							+ "                \"showTooltip\": \"0\",\n" + "                \"decimals\": \"0\",\n"
-							+ "                \"captionFontSize\": \"14\",\n"
-							+ "                \"subcaptionFontSize\": \"14\",\n"
-							+ "                \"subcaptionFontBold\": \"0\"\n" + "            },\n"
-							+ "            \"data\": [\n" + "                {\n"
-							+ "                    \"label\": \"Food\",\n"
-							+ "                    \"value\": \"28504\"\n" + "                }, \n"
-							+ "                {\n" + "                    \"label\": \"Apparels\",\n"
-							+ "                    \"value\": \"14633\"\n" + "                }, \n"
-							+ "                {\n" + "                    \"label\": \"Electronics\",\n"
-							+ "                    \"value\": \"10507\"\n" + "                }, \n"
-							+ "                {\n" + "                    \"label\": \"Household\",\n"
-							+ "                    \"value\": \"4910\"\n" + "                }\n" + "            ]\n"
-							+ "        }\n" + "    }).render();\n" + "});\n" + "</script>\n" + "</head>\n" + "<body>\n"
-							+ "  <div id=\"chart-container\">FusionCharts XT will load here!</div>\n"
-							+ "<div> Loaded from a string </div>\n" + "</body>\n" + "</html>");
+							+ "<script type=\"text/javascript\" src=\"" + urlFusionchartsjs.toString() + "\"></script>\n" 
+							+ "<script type=\"text/javascript\" src=\""+ urlFusionchartsthemejs.toString() + "\"></script>\n"
+							+ "<script type=\"text/javascript\">\n" +
+							
+							//your fusioncharts method goes here:
+							"FusionCharts.ready(function () {\n" +
+				             "    var revenueChart = new FusionCharts({\n" +
+				             "        type: 'doughnut2d',\n" +
+				             "        renderAt: 'chart-container',\n" +
+				             "        width: '450',\n" +
+				             "        height: '350',\n" +
+				             "        dataFormat: 'json',\n" +
+				             "        dataSource: {\n" +
+				             "            \"chart\": {\n" +
+				             "                \"caption\": \"Split of Revenue by Product Categories\",\n" +
+				             "                \"subCaption\": \"Last year\",\n" +
+				             "                \"numberPrefix\": \"$\",\n" +
+				             "                \"paletteColors\": \"#0075c2,#1aaf5d,#f2c500,#f45b00,#8e0000\",\n" +
+				             "                \"bgColor\": \"#ffffff\",\n" +
+				             "                \"showBorder\": \"0\",\n" +
+				             "                \"use3DLighting\": \"0\",\n" +
+				             "                \"showShadow\": \"0\",\n" +
+				             "                \"enableSmartLabels\": \"0\",\n" +
+				             "                \"startingAngle\": \"310\",\n" +
+				             "                \"showLabels\": \"0\",\n" +
+				             "                \"showPercentValues\": \"1\",\n" +
+				             "                \"showLegend\": \"1\",\n" +
+				             "                \"legendShadow\": \"0\",\n" +
+				             "                \"legendBorderAlpha\": \"0\",\n" +
+				             "                \"defaultCenterLabel\": \"Total revenue: $64.08K\",\n" +
+				             "                \"centerLabel\": \"Revenue from $label: $value\",\n" +
+				             "                \"centerLabelBold\": \"1\",\n" +
+				             "                \"showTooltip\": \"0\",\n" +
+				             "                \"decimals\": \"0\",\n" +
+				             "                \"captionFontSize\": \"14\",\n" +
+				             "                \"subcaptionFontSize\": \"14\",\n" +
+				             "                \"subcaptionFontBold\": \"0\"\n" +
+				             "            },\n" +
+				             "            \"data\": [\n" +
+				             "                {\n" +
+				             "                    \"label\": \"Food\",\n" +
+				             "                    \"value\": \"28504\"\n" +
+				             "                }, \n" +
+				             "                {\n" +
+				             "                    \"label\": \"Apparels\",\n" +
+				             "                    \"value\": \"14633\"\n" +
+				             "                }, \n" +
+				             "                {\n" +
+				             "                    \"label\": \"Electronics\",\n" +
+				             "                    \"value\": \"10507\"\n" +
+				             "                }, \n" +
+				             "                {\n" +
+				             "                    \"label\": \"Household\",\n" +
+				             "                    \"value\": \"4910\"\n" +
+				             "                }\n" +
+				             "            ]\n" +
+				             "        }\n" +
+				             "    }).render();\n" +
+				             "});\n" 
+							
+							+ "</script>\n" 
+							+ "</head>\n" 
+							+ "<body>\n"
+							
+							+ "<div id=\"chart-container\">FusionCharts XT will load here!</div>\n"
+							+ "<div> Loaded from a string </div>\n" 
+							
+							+ "</body>\n" 
+							+ "</html>");
 				} catch (MalformedURLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -137,14 +168,16 @@ public class Fusioncharts_javafx extends Application {
 				try {
 					urlFusionchartsjs = fileFusionchartsjs.toURI().toURL();
 					urlFusionchartsthemejs = fileFusionchartschartsjs.toURI().toURL();
-					engine.loadContent("<html>\n" + "<head>\n"
+					engine.loadContent("<html>\n" 
+							+ "<head>\n"
 							+ "<title>My first chart using FusionCharts Suite XT</title>\n"
 							+ "<script type=\"text/javascript\" src=\"" + urlFusionchartsjs.toString()
 							+ "\"></script>\n" + "<script type=\"text/javascript\" src=\""
 							+ urlFusionchartsthemejs.toString() + "\"></script>\n"
-							+ "<script type=\"text/javascript\">\n" 
-							
-							+ "FusionCharts.ready(function () {\r\n" + 
+							+ "<script type=\"text/javascript\">\n" +
+
+							//your fusioncharts method goes here:
+							"FusionCharts.ready(function () {\r\n" + 
 							"    var analysisChart = new FusionCharts({\r\n" + 
 							"        type: 'stackedColumn3DLine',\r\n" + 
 							"        renderAt: 'chart-container',\r\n" + 
